db951fef2028b6cee9a69b9b99076bba5b594628
ashshelby <nepalidude3@gmail.com>
2022-03-13 21:51:13 +0545

    last changes

--- resources/views/post/profile.blade.php
+++ resources/views/post/profile.blade.php
@@ -1,11 +1,18 @@
 <html>
 <head>
     <title>{{" $user->username | EverGreen."}}</title>
+    <link rel="stylesheet" href="{{ asset('css/profile.css') }}">
 </head>
 
 <body>
-<h2>{{$user->username}}</h2>
-<h3>{{$user->name }}</h3>
+	<div class="username-container__main">
+		<h2 class="username-main__main">{{$user->username}}</h2>
+		<br>
+		<br>
+		<div class="name-profile__container">
+			<h3 class="name-profile__main">{{$user->name }}</h3>
+		</div>
+	</div>
 <h4>{{"$user->username's Posts:"}}</h4>
 @foreach($posts as $post)
     <div style="margin:10px;">
